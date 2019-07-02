1a76c406d59b2a58176ed679c1d22b04a297bce0
Ubuntu <ubuntu@ip-172-31-84-134.ec2.internal>
2019-07-02 09:11:21 +0000

    Added append line feature on HTML, required to reload with each new search

--- scripts/pokeapi.js
+++ scripts/pokeapi.js
@@ -1,13 +1,19 @@
 /* global axios */
+/* global $ */
 
 function NameSearch(){
-    let name = document.getElementById("NameInput").value;
+    let name = document.getElementById("NameInput").value.toLowerCase();
     //PokeAPI api works by having the pokemon name right behind the URL
     let pokemonurl = "https://pokeapi.co/api/v2/pokemon/" + name;
     
     axios.get(pokemonurl)
         .then(function(response){
             console.log(response.data)
-            console.log(response.data.id)
+            // console.log(response.data.name)
+            let pokemonname = response.data.name;
+            console.log(response.data.types[0].type.name)
+            console.log(response.data.id);
+            
+                $("#PokemonName").append(`Name : ${pokemonname}`);
         })
 }
