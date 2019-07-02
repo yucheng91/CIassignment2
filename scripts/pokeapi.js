695f5fb60e936128c135d24881ffc6a40ea42663
Ubuntu <ubuntu@ip-172-31-84-134.ec2.internal>
2019-07-02 08:05:14 +0000

    set up Axios

--- scripts/pokeapi.js
+++ scripts/pokeapi.js
@@ -1,2 +1,13 @@
 /* global axios */
 
+function NameSearch(){
+    let name = document.getElementById("NameInput").value;
+    //PokeAPI api works by having the pokemon name right behind the URL
+    let pokemonurl = "https://pokeapi.co/api/v2/pokemon/" + name;
+    
+    axios.get(pokemonurl)
+        .then(function(response){
+            console.log(response.data)
+            console.log(response.data.id)
+        })
+}
