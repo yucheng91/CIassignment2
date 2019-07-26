0d11f5f1aad7b4e8cce03678c4802b29513098f6
Ubuntu <ubuntu@ip-172-31-84-134.ec2.internal>
2019-07-26 09:57:28 +0000

    Added additional Jasmine Test Scenario

--- test/specs/poketest.js
+++ test/specs/poketest.js
@@ -1,23 +1,43 @@
 describe("Pokemon Search", function() {
     describe('Name Search', function() {
-        it('should return valid pokemon url', function() {
+        it('should return valid pokemon url of name Rattata', function() {
             let name = "rattata";
             let pokemonurl = "https://pokeapi.co/api/v2/pokemon/" + name;
             expect(pokemonurl).toEqual("https://pokeapi.co/api/v2/pokemon/rattata");
         });
     });
     describe('index Search', function() {
-        it('should return valid pokemon url', function() {
+        it('should return valid pokemon url of index 382', function() {
             let index = "382";
             let pokemonurl = "https://pokeapi.co/api/v2/pokemon/" + index;
             expect(pokemonurl).toEqual("https://pokeapi.co/api/v2/pokemon/382");
         });
     });
-    describe('wrong name Search', function() {
-        it('should return valid pokemon url', function() {
-            let index = "mickeymouse";
-            let pokemonurl = "https://pokeapi.co/api/v2/pokemon/" + index;
-            expect(pokemonurl).toBeDefined();
+});
+
+describe("Pokemon Axios", function() {
+    describe("Fetch Pokemon Name (Name)", function() {
+        it("should be able to fetch same name based on valid Pokemon name input (pikachu)", function(done) {
+            let name = "pikachu";
+            let pokemonurl = "https://pokeapi.co/api/v2/pokemon/" + name;
+
+            axios.get(pokemonurl)
+                .then(function(response) {
+                    expect(response.data.name).toBe("pikachu");
+                    done();
+                });
         });
     });
-});
+    describe("Fetch Pokemon Name (Index)", function() {
+        it("should be able to fetch same name based on valid Pokemon index input (#123 - scyther)", function(done) {
+            let index = "123"; //Pokemon #123 is Scyther
+            let pokemonurl = "https://pokeapi.co/api/v2/pokemon/" + index;
+
+            axios.get(pokemonurl)
+                .then(function(response) {
+                    expect(response.data.name).toBe("scyther");
+                    done();
+                });
+        });
+    })
+})
